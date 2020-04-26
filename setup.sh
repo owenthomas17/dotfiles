fb08b000a89916722d7db4ac26aea83204d292e4
owen t <2owen.thomas@gmail.com>
2020-04-26 11:40:24 +0100

    Refactor dotfile creation into a function

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Helper functions
-
-log () {
-    echo "$(date -u) - $1"
-}
-
 # Globals
 GIT_DIRECTORY=~/repos/dotfiles
 VIMGO_DIR=~/.vim/pack/plugins/start/vim-go
 VIMGO_GIT_REPO=https://github.com/fatih/vim-go.git
+
+log () {
+    echo "$(date -u) - $1"
+}
 
 # Check if git exists
 if [ -d "$GIT_DIRECTORY" ]; then
@@ -19,65 +17,34 @@ else
     exit 1
 fi
 
+create_dotfile () {
+    local DOTFILE="$HOME/${1}"
+    local GIT_DOTFILE=$1
+
+    if [ -L "$DOTFILE" ]; then
+        log "$DOTFILE symlink already exists moving on..."
+    else
+        if [ -f "$DOTFILE" ]; then
+            log "removing $DOTFILE"
+            rm $DOTFILE
+            log "creating symlink for $DOTFILE in git repo"    
+            ln -s ${GIT_DIRECTORY}/${GIT_DOTFILE} $DOTFILE 
+        elif [ ! -L "$DOTFILE" ]; then
+            log "creating symlink for $DOTFILE in git repo"    
+            ln -s ${GIT_DIRECTORY}/${GIT_DOTFILE} $DOTFILE 
+        fi
+    fi
+
+}
+
+
 # Setup symlinks into home folder
 
-if [ -L ~/.bashrc ]; then
-    log "~/.bashrc symlink already exists moving on..."
-else
-    if [ -f ~/.bashrc ]; then
-        log "removing ~/.bashrc"
-        rm ~/.bashrc
-        log "creating symlink for ~/.bashrc in git repo"    
-        ln -s ${GIT_DIRECTORY}/.bashrc ~/.bashrc 
-    elif [ ! -L ~/.bashrc ]; then
-        log "creating symlink for ~/.bashrc in git repo"    
-        ln -s ${GIT_DIRECTORY}/.bashrc ~/.bashrc 
-    fi
-fi
+DOTFILES=".bashrc .vimrc .profile .tmux.conf"
 
-if [ -L ~/.vimrc ]; then
-    log "~/.vimrc symlink already exists moving on..."
-else
-    if [ -f ~/.vimrc ]; then
-        log "removing ~/.vimrc"
-        rm ~/.vimrc
-        log "creating symlink for ~/.vimrc in git repo"    
-        ln -s ${GIT_DIRECTORY}/.vimrc ~/.vimrc 
-    elif [ ! -L ~/.vimrc ]; then
-        log "creating symlink for ~/.vimrc in git repo"    
-        ln -s ${GIT_DIRECTORY}/.vimrc ~/.vimrc
-    fi
-fi
-
-# Install .profile
-if [ -L ~/.profile ]; then
-    log "~/.profile symlink already exists moving on..."
-else
-    if [ -f ~/.profile ]; then
-        log "removing ~/.profile"
-        rm ~/.profile
-        log "creating symlink for ~/.profile in git repo"    
-        ln -s ${GIT_DIRECTORY}/.profile ~/.profile 
-    elif [ ! -L ~/.profile ]; then
-        log "creating symlink for ~/.profile in git repo"    
-        ln -s ${GIT_DIRECTORY}/.profile ~/.profile
-    fi
-fi
-
-# Install .tmux.conf
-if [ -L ~/.tmux.conf ]; then
-    log "~/.tmux.conf symlink already exists moving on..."
-else
-    if [ -f ~/.tmux.conf ]; then
-        log "removing ~/.tmux.conf"
-        rm ~/.tmux.conf
-        log "creating symlink for ~/.tmux.conf in git repo"
-        ln -s ${GIT_DIRECTORY}/.tmux.conf ~/.tmux.conf
-    elif [ ! -L ~/.tmux.conf ]; then
-        log "creating symlink for ~/.tmux.conf in git repo"
-        ln -s ${GIT_DIRECTORY}/.tmux.conf ~/.tmux.conf
-    fi
-fi
+for DOTFILE in $DOTFILES; do
+    create_dotfile $DOTFILE
+done
 
 # Make sure vim plugins are downloaded
 if [ -d "$VIMGO_DIR" ]; then
@@ -93,4 +60,3 @@ else
         log "vim-go cloned successfully from $VIMGO_GIT_REPO"
     fi
 fi
-
