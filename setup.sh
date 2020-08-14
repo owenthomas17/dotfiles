c578e5ba643a084ef7caf2f62a0f636f6046d360
owen t <2owen.thomas@gmail.com>
2020-08-14 12:11:14 +0100

    refactor into functions

--- setup.sh
+++ setup.sh
@@ -10,15 +10,7 @@ log () {
     echo "$(date -u) - $1"
 }
 
-# Check if git exists
-if [ -d "$GIT_DIRECTORY" ]; then
-    log "Git repo is present, continuing setup"
-else 
-    log "Git repo is not present. The dotfiles repo should be cloned to ~/repos/, exiting..."
-    exit 1
-fi
-
-create_dotfile () {
+createDotfile () {
     local DOTFILE="$HOME/${1}"
     local GIT_DOTFILE=$1
 
@@ -38,46 +30,72 @@ create_dotfile () {
 
 }
 
+installVimPlugins () {
 
-# Setup symlinks into home folder
+  # Make sure vim plugins are downloaded
+  if [ -d "$VIMGO_DIR" ]; then
+      log "vim-go already exists checking it's up to date in dir: ${VIMGO_DIR}"
+      git -C ${VIMGO_DIR} pull --allow-unrelated-histories --quiet
+  else
+      log "vim-go doesn't exist in '${VIMGO_DIR}', git cloning"
+      git clone ${VIMGO_GIT_REPO} ${VIMGO_DIR} --quiet
+      RESULT=$?
+      if [ "$RESULT" -gt 0 ]; then
+          log "Cloning $VIMGO_GIT_REPO failed. Exiting..."
+          exit 1
+      else
+          log "vim-go cloned successfully from $VIMGO_GIT_REPO"
+      fi
+  fi
 
-DOTFILES=".bashrc .vimrc .profile .tmux.conf .Xresources .dircolors"
+  if [ -d "$COC_DIR" ]; then
+      log "coc vim directory already exists, checking it's up to date in dir: ${COC_DIR}"
+      cd $COC_DIR
+      curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
+      cd ${GIT_DIRECTORY}
+  else
+      log "coc vim doesn't exist in '${COC_DIR}', git cloning"
+      cd $COC_DIR
+      curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
+      RESULT=$?
+      if [ "$RESULT" -gt 0 ]; then
+          log "Downloading $COC_RELEASE_REPO failed. Exiting..."
+          exit 1
+      else
+          log "coc downloaded successfully from $COC_RELEASE_REPO"
+      fi
+      cd ${GIT_DIRECTORY}
+  fi
 
-for DOTFILE in $DOTFILES; do
-    create_dotfile $DOTFILE
-done
+}
 
-# Make sure vim plugins are downloaded
-if [ -d "$VIMGO_DIR" ]; then
-    log "vim-go already exists checking it's up to date in dir: ${VIMGO_DIR}"
-    git -C ${VIMGO_DIR} pull --allow-unrelated-histories --quiet
-else
-    log "vim-go doesn't exist in '${VIMGO_DIR}', git cloning"
-    git clone ${VIMGO_GIT_REPO} ${VIMGO_DIR} --quiet
-    RESULT=$?
-    if [ "$RESULT" -gt 0 ]; then
-        log "Cloning $VIMGO_GIT_REPO failed. Exiting..."
-        exit 1
-    else
-        log "vim-go cloned successfully from $VIMGO_GIT_REPO"
-    fi
-fi
 
-if [ -d "$COC_DIR" ]; then
-    log "coc vim directory already exists, checking it's up to date in dir: ${COC_DIR}"
-    cd $COC_DIR
-    curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
-    cd ${GIT_DIRECTORY}
-else
-    log "coc vim doesn't exist in '${COC_DIR}', git cloning"
-    cd $COC_DIR
-    curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
-    RESULT=$?
-    if [ "$RESULT" -gt 0 ]; then
-        log "Downloading $COC_RELEASE_REPO failed. Exiting..."
-        exit 1
-    else
-        log "coc downloaded successfully from $COC_RELEASE_REPO"
-    fi
-    cd ${GIT_DIRECTORY}
-fi
+gitDirExists () {
+  # Check if git exists
+  if [ -d "$GIT_DIRECTORY" ]; then
+      log "Git repo is present, continuing setup"
+  else
+      log "Git repo is not present. The dotfiles repo should be cloned to ~/repos/, exiting..."
+      exit 1
+  fi
+}
+
+createDotfiles () {
+
+  # Setup symlinks into home folder
+  DOTFILES=".bashrc .vimrc .profile .tmux.conf .Xresources .dircolors"
+
+  for DOTFILE in $DOTFILES; do
+     createDotfile $DOTFILE
+  done
+
+}
+
+
+main () {
+  gitDirExists
+  createDotfiles
+  installVimPlugins
+}
+
+main
