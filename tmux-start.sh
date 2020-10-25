09d7af4e247f6f8f15bd4ec1872b258939281afb
owen t <2owen.thomas@gmail.com>
2020-10-25 20:21:03 +0000

    adding comments

--- tmux-start.sh
+++ tmux-start.sh
@@ -7,7 +7,10 @@
 tmux new-session -s dev -n shell -d
 
 # Create additional windows
+# Window name (-n)
+# Change working directory (-c)
 tmux new-window -n vim -c ${HOME}/repos
 
 # Attach to the created session
+# Attach to session name (-t)
 tmux attach -t dev
