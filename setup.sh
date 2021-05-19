7446ef3b24e383576af24eff6d78e7c8c20cb6b0
owen t <2owen.thomas@gmail.com>
2021-05-19 23:16:27 +0100

    New VIM plugins and lsp
    Install new VIM plugins and install a new LSP for python. This is to be
    used with ALE

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ VIMGO_DIR="${HOME}/.vim/pack/plugins/start/vim-go"
 VIMGO_GIT_REPO="git@github.com:fatih/vim-go.git"
 COC_DIR="${HOME}/.vim/pack/coc/start/coc.nvim-release"
 COC_RELEASE_REPO="https://github.com/neoclide/coc.nvim/archive/release.tar.gz"
+VIM_POLYGLOT_DIR="${HOME}/.vim/pack/plugins/start/vim-polyglot"
+VIM_POLYGLOT_REPO="https://github.com/sheerun/vim-polyglot"
+VIM_ALE_REPO="https://github.com/dense-analysis/ale.git"
+VIM_ALE_DIR="${HOME}/vim/pack/plugins/start/ale"
+VIM_GRUVBOX_DIR_REPO="https://github.com/morhetz/gruvbox.git"
+VIM_GRUVBOX_DIR="${HOME}/.vim/pack/default/start/gruvbox"
 
 log () {
     echo "$(date -u) - $1"
@@ -31,47 +37,48 @@ createDotfile () {
 
 }
 
-installVimPlugins () {
+installLanguageServers () {
 
+    which pyls > /dev/null
+    PYLS_INSTALLED=$?
+    if [ $PYLS_INSTALLED -eq 1 ]; then
+        log "Installing python language server"
+        pip3 install python-language-server
+    else
+        log "Python language server already installed"
+    fi
+
+}
+
+installVimPlugins () {
     # Create .vim dir if it doesn't exist
     if [ ! -d ${HOME}/.vim ]; then
         log "${HOME}/.vim directory doesn't exist. Creating it"
         mkdir -p ${HOME}/.vim
     fi
 
-    # Make sure vim plugins are downloaded
-    if [ -d "$VIMGO_DIR" ]; then
-        log "vim-go already exists checking it's up to date in dir: ${VIMGO_DIR}"
-        git -C ${VIMGO_DIR} pull --allow-unrelated-histories --quiet
+    # Install color scheme
+    if [ ! -d ${VIM_GRUVBOX_DIR} ]; then
+        log "Installing gruvbox"
+        git clone ${VIM_GRUVBOX_DIR_REPO} ${VIM_GRUVBOX_DIR}
     else
-        log "vim-go doesn't exist in '${VIMGO_DIR}', git cloning"
-        git clone ${VIMGO_GIT_REPO} ${VIMGO_DIR} --quiet
-        RESULT=$?
-        if [ "$RESULT" -gt 0 ]; then
-            log "Cloning $VIMGO_GIT_REPO failed. Exiting..."
-            exit 1
-        else
-            log "vim-go cloned successfully from $VIMGO_GIT_REPO"
-        fi
+        log "gruvbox already installed at ${VIM_GRUVBOX_DIR}"
     fi
 
-    if [ -d "$COC_DIR" ]; then
-        log "coc vim directory already exists, checking it's up to date in dir: ${COC_DIR}"
-        cd $COC_DIR
-        curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
-        cd ${GIT_DIRECTORY}
+
+    # Install Syntax highlighting
+    if [ ! -d ${VIM_POLYGLOT_DIR} ]; then
+        log "Installing vim-polyglot"
+        git clone --depth 1 ${VIM_POLYGLOT_REPO} ${VIM_POLYGLOT_DIR}
     else
-        log "coc vim doesn't exist in '${COC_DIR}', git cloning"
-        cd $COC_DIR
-        curl --fail -L ${COC_RELEASE_REPO} | tar xzfv -
-        RESULT=$?
-        if [ "$RESULT" -gt 0 ]; then
-            log "Downloading $COC_RELEASE_REPO failed. Exiting..."
-            exit 1
-        else
-            log "coc downloaded successfully from $COC_RELEASE_REPO"
-        fi
-        cd ${GIT_DIRECTORY}
+        log "vim-polyglot already installed at ${VIM_POLYGLOT_DIR}"
+    fi
+
+    if [ ! -d ${VIM_ALE_DIR} ]; then
+        log "Installing vim ale"
+        git clone --depth 1 ${VIM_ALE_REPO} ${VIM_ALE_DIR}
+    else
+        log "Ale already installed at ${VIM_ALE_DIR}"
     fi
 
 }
@@ -102,6 +109,7 @@ createDotfiles () {
 main () {
     gitDirExists
     createDotfiles
+    installLanguageServers
     installVimPlugins
 }
 
