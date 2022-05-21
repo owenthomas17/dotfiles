7cf261956ffdfc80bca833f81571c5ba13c1fc3a
Owen Thomas <owen@owen-thomas.co.uk>
2022-05-21 13:23:04 +0100

    setup-nvim: Add download nvim deb package function

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+
+NVIM_DEBIAN_PACKAGE_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb"
+NVIM_DEBIAN_PACKAGE_SHA_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb.sha256sum"
+
 log () {
     echo "$(date -u) - $1"
 }
 
+downloadNvim() {
+	log "Downloading nvim debian package..."
+	wget -O /tmp/nvim-linux64.deb --quiet "${NVIM_DEBIAN_PACKAGE_LINK}"
+
+	log "Downloading nvim debian package sha256..."
+	wget -O /tmp/nvim-linux64.deb.sha256sum --quiet "${NVIM_DEBIAN_PACKAGE_SHA_LINK}"
+
+	log "Validating sha256..."
+	cd /tmp
+	sha256sum -c /tmp/nvim-linux64.deb.sha256sum
+	if [[ $? -gt 0 ]]; then
+	    log "Checksum failed, please try re-downloading the file"
+	    exit 1
+	fi
+	cd -
+	log "sha256 validated and correct..."
+}
+
 main () {
-    log "Not yet implemented nvim installation, skipping..."
+    downloadNvim
 }
 
 main
