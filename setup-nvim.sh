feebd4fd8cab597b5b4a68d0ac4e90998b06f1f7
Owen Thomas <owen@owen-thomas.co.uk>
2022-05-21 13:37:07 +0100

    setup-nvim: Add install and cleanup functions

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
 NVIM_DEBIAN_PACKAGE_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb"
 NVIM_DEBIAN_PACKAGE_SHA_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb.sha256sum"
+NVIM_DEBIAN_PACKAGE_FILEPATH="/tmp/nvim-linux64.deb"
+NVIM_DEBIAN_PACKAGE_SHA_FILEPATH="/tmp/nvim-linux64.deb.sha256sum"
 
 log () {
     echo "$(date -u) - $1"
@@ -10,14 +11,14 @@ log () {
 
 downloadNvim() {
 	log "Downloading nvim debian package..."
-	wget -O /tmp/nvim-linux64.deb --quiet "${NVIM_DEBIAN_PACKAGE_LINK}"
+	wget -O "${NVIM_DEBIAN_PACKAGE_FILEPATH}" --quiet "${NVIM_DEBIAN_PACKAGE_LINK}"
 
 	log "Downloading nvim debian package sha256..."
-	wget -O /tmp/nvim-linux64.deb.sha256sum --quiet "${NVIM_DEBIAN_PACKAGE_SHA_LINK}"
+	wget -O "${NVIM_DEBIAN_PACKAGE_SHA_FILEPATH}" --quiet "${NVIM_DEBIAN_PACKAGE_SHA_LINK}"
 
 	log "Validating sha256..."
 	cd /tmp
-	sha256sum -c /tmp/nvim-linux64.deb.sha256sum
+	sha256sum -c "${NVIM_DEBIAN_PACKAGE_SHA_FILEPATH}"
 	if [[ $? -gt 0 ]]; then
 	    log "Checksum failed, please try re-downloading the file"
 	    exit 1
@@ -26,8 +27,24 @@ downloadNvim() {
 	log "sha256 validated and correct..."
 }
 
+cleanupFiles() {
+    log "Removing all temporary files"
+    for file in "${NVIM_DEBIAN_PACKAGE_FILEPATH}" "${NVIM_DEBIAN_PACKAGE_SHA_FILEPATH}"; do
+	    log "Removing ${file}"
+	    rm -rf "${file}"
+    done
+}
+
+installNvim() {
+    log "Installing nvim from debian package"
+    sudo apt-get -qq install "${NVIM_DEBIAN_PACKAGE_FILEPATH}"
+    log "Installed nvim correctly"
+}
+
 main () {
     downloadNvim
+    installNvim
+    cleanupFiles
 }
 
 main
