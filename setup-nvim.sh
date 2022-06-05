a05575ef93cc0984d3c14c7b4d95c3dc9ef8cc37
Owen Thomas <owen@owen-thomas.co.uk>
2022-06-05 12:43:44 +0100

    setup-nvim: Templated nvim version. Added more helper functions
    Added a cleanupFiles helper function to remove any downloaded files. Added a helper function to detect existing installs, won't do anything unless desired version matches the currently installed version. Made directory changes explicit in install function. Condensed logic statement down to one line as appropriate

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-NVIM_DEBIAN_PACKAGE_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb"
-NVIM_DEBIAN_PACKAGE_SHA_LINK="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb.sha256sum"
+NVIM_VERSION="v0.7.0"
+NVIM_DEBIAN_PACKAGE_LINK="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux64.deb"
+NVIM_DEBIAN_PACKAGE_SHA_LINK="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux64.deb.sha256sum"
 NVIM_DEBIAN_PACKAGE_FILEPATH="/tmp/nvim-linux64.deb"
 NVIM_DEBIAN_PACKAGE_SHA_FILEPATH="/tmp/nvim-linux64.deb.sha256sum"
+NVIM_CONFIG_DIRECTORY="${HOME}/.config/nvim"
+NVIM_CONFIG_FILE_PATH="${HOME}/.config/nvim/init.lua"
 
 log () {
     echo "$(date -u) - $1"
 }
 
 downloadNvim() {
+	local WORKDIR=$(pwd)
 	log "Downloading nvim debian package..."
 	wget -O "${NVIM_DEBIAN_PACKAGE_FILEPATH}" --quiet "${NVIM_DEBIAN_PACKAGE_LINK}"
 
@@ -18,12 +22,8 @@ downloadNvim() {
 
 	log "Validating sha256..."
 	cd /tmp
-	sha256sum -c "${NVIM_DEBIAN_PACKAGE_SHA_FILEPATH}"
-	if [[ $? -gt 0 ]]; then
-	    log "Checksum failed, please try re-downloading the file"
-	    exit 1
-	fi
-	cd -
+	sha256sum -c "${NVIM_DEBIAN_PACKAGE_SHA_FILEPATH}" || log "Checksum failed, please try re-downloading the file"; exit 1
+	cd "${WORKDIR}"
 	log "sha256 validated and correct..."
 }
 
@@ -39,12 +39,30 @@ installNvim() {
     log "Installing nvim from debian package"
     sudo apt-get -qq install "${NVIM_DEBIAN_PACKAGE_FILEPATH}"
     log "Installed nvim correctly"
+    cleanupFiles
+}
+
+installDotfile() {
+    ln -s init.lua "$DOTFILE"
+}
+
+detectPreviousInstall() {
+    local CURRENT_NVIM_VERSION=$(nvim -v | head -n1 | awk '{print $2}')
+    if [[ $CURRENT_NVIM_VERSION == $NVIM_VERSION ]]; then
+	    log "Skipping new nvim installation because nvim has been detected as already installed, and at the desired version: ${NVIM_VERSION}"
+	    return 0
+    fi
+
+    log "Nvim not found, or doesn't match desired version"
+    return 1
 }
 
 main () {
-    downloadNvim
-    installNvim
-    cleanupFiles
+    if ! detectPreviousInstall; then
+	downloadNvim
+	installNvim
+    fi
+    installDotfile
 }
 
 main
