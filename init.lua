6a0b4e89aafd0d47e16dc7a630658fdc03ac05b6
Owen Thomas <owen@owen-thomas.co.uk>
2022-06-05 12:55:02 +0100

    init.lua: bringing nvim config into vcs

---
+++ init.lua
@@ -0,0 +1,104 @@
+vim.o.termguicolors = true
+vim.o.syntax = 'on'
+vim.errorbells = false
+vim.o.smartcase = true
+vim.o.showmode = false
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.g.mapleader = ' '
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.cmd([[autocmd vimenter * ++nested colorscheme gruvbox]])
+
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf"
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --crlf",
+    ["*"] = "win32yank.exe -o --crlf"
+  },
+  cache_enable = 0,
+}
+
+-- Custom mappings
+
+---- Custom macros
+vim.keymap.set('n', '<leader>t', ':belowright 10split<CR>:terminal<CR>i')
+vim.keymap.set('n', '<leader>e', ':Vexplore<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+---- Navigation overrides for normal mode.
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
+
+-- lua version of vim script VimPlug
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('neovim/nvim-lspconfig')
+Plug('morhetz/gruvbox')
+vim.call('plug#end')
+
+
+local lsp = require('lspconfig')
+lsp.gopls.setup {}
+lsp.pyright.setup {}
+
+local nvim_lsp = require('lspconfig')
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'gopls', 'pyright', 'clangd' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
